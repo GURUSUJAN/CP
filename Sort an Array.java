64fb1003dacbc8acbf94f27995491abd4c6ec1c7
GURUSUJAN REDDY <94339643+GURUSUJAN@users.noreply.github.com>
2023-03-01 11:13:21 +0530

    https://leetcode.com/problems/sort-an-array/

---
+++ Sort an Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortArray(int[] nums) 
+    {
+        // int n=nums.length;
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=i;j<n;j++)
+        //     {
+        //         if(nums[i]>nums[j])
+        //         {
+        //             swap(nums[i],nums[j]);
+        //         }
+        //     }
+        // }
+        // return nums;
+        Arrays.sort(nums);
+        return nums;
+    }
+}
